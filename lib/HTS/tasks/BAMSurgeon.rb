2ca3b0be7bbff120e757f513f7ff0c60f948c2b0
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2019-07-25 11:12:47 +0200

    Added support to BAMSurgeon SNVs and SVs

--- lib/HTS/tasks/BAMSurgeon.rb
+++ lib/HTS/tasks/BAMSurgeon.rb
@@ -1,28 +1,85 @@
 require 'tools/BAMSurgeon'
 module HTS
 
-  input :bed, :file, "variants file in bed format", :nofile => true
-  input :bam, :file, "BAM", nil, :nofile => true
+  input :varfile, :file, "variants file in bed format", :nofile => true
+  input :bamfile, :file, "BAM", :nofile => true
+  input :outbam, :file, "Output BAM", nil, :nofile => true
   input :reference, :select, "Reference code", "b37", :select_options => %w(b37 hg19 hg38 GRCh38 hs37d5), :nofile => true
-  input :snv_frac, :float, "Maximum allowable linked SNP MAF (for avoiding haplotypes)", 1
-  input :mut_frac, :float, "Allelic fraction at which to make SNVs", 0.5
-  input :num_snvs, :float, "maximum number of mutations to try (default: entire input)", nil
-  input :cnv_file, :file, "tabix-indexed list of genome-wide absolute copy number values", nil, :nofile => true
-  input :cover_diff, :float, "allow difference in input and output coverage", 0.1
+  input :snvfrac, :float, "Maximum allowable linked SNP MAF (for avoiding haplotypes)", 1
+  input :mutfrac, :float, "Allelic fraction at which to make SNVs", 0.5
+  input :numsnvs, :float, "maximum number of mutations to try (default: entire input)", nil
+  input :cnvfile, :file, "tabix-indexed list of genome-wide absolute copy number values", nil, :nofile => true
+  input :coverdiff, :float, "allow difference in input and output coverage", 0.1
   input :procs, :float, "split into multiple processes", 1
-  input :picard_jar, :file, "path to picard.jar", nil, :nofile => true
-  input :min_depth, :float, "minimum read depth to make mutation", 10
-  input :max_depth, :float, "maximum read depth to make mutation", 2000
-  input :min_mut_threads, :float , "minimum number of mutated reads to output per site", nil
-  input :avoid_reads, :file, "file of read names to avoid (mutations will be skipped if overlap)", nil, :nofile => true
+  input :picardjar, :file, "path to picard.jar", nil, :nofile => true
+  input :mindepth, :float, "minimum read depth to make mutation", 10
+  input :maxdepth, :float, "maximum read depth to make mutation", 2000
+  input :minmutreads, :float , "minimum number of mutated reads to output per site", nil
+  input :avoidreads, :file, "file of read names to avoid (mutations will be skipped if overlap)", nil, :nofile => true
   input :aligner, :select, "aligner", "mem", :select_options => %w(mem backtrack novoalign gsnap STAR bowtie2 tmap bwakit), :nofile => true
 
-  task :BAMSurgeon_add_indels => :binary do |bed,bam,reference,snv_frac,mut_frac,num_snvs,cnv_file,cover_diff,procs, picard_jar,min_depth,max_depth,min_mut_threads,avoid_reads,aligner|
-    output = self.tmp_path
+  task :BAMSurgeon_add_indels => :binary do |varfile,bamfile,outbam,reference,snvfrac,mutfrac,numsnvs,cnvfile,coverdiff,procs,picardjar,mindepth,maxdepth,minmutreads,avoidreads,aligner|
+    if outbam.nil?
+      inputs[:outbam]= self.tmp_path
+    end
     reference = reference_file reference
     bam = Samtools.prepare_BAM(bam) if bam
     reference = Samtools.prepare_FASTA(reference)
-    BAMSurgeon.add_indels(bed,bam,reference,output,snv_frac,mut_frac,num_snvs,cnv_file,cover_diff,procs,picard_jar,min_depth,max_depth,min_mut_threads,avoid_reads,aligner)  
+    inputs[:reference] = reference
+    BAMSurgeon.add_indels(inputs)
   end
 
+  input :varfile, :file, "variants file in bed format", :nofile => true
+  input :bamfile, :file, "BAM", :nofile => true
+  input :outbam, :file, "Output BAM", nil
+  input :reference, :select, "Reference code", "b37", :select_options => %w(b37 hg19 hg38 GRCh38 hs37d5), :nofile => true
+  input :snvfrac, :float, "Maximum allowable linked SNP MAF (for avoiding haplotypes)", 1
+  input :mutfrac, :float, "Allelic fraction at which to make SNVs", 0.5
+  input :numsnvs, :float, "maximum number of mutations to try (default: entire input)", nil
+  input :cnvfile, :file, "tabix-indexed list of genome-wide absolute copy number values", nil, :nofile => true
+  input :coverdiff, :float, "allow difference in input and output coverage", 0.1
+  input :haplosize, :float, "haplotype size", 0
+  input :procs, :float, "split into multiple processes", 1
+  input :picardjar, :file, "path to picard.jar", nil, :nofile => true
+  input :mindepth, :float, "minimum read depth to make mutation", 10
+  input :maxdepth, :float, "maximum read depth to make mutation", 2000
+  input :minmutreads, :float , "minimum number of mutated reads to output per site", nil
+  input :avoidreads, :file, "file of read names to avoid (mutations will be skipped if overlap)", nil, :nofile => true
+  input :aligner, :select, "aligner", "mem", :select_options => %w(mem backtrack novoalign gsnap STAR bowtie2 tmap bwakit), :nofile => true
+
+  task :BAMSurgeon_add_snvs => :binary do |varfile,bamfile,outbam,reference,snvfrac,mutfrac,numsnvs,cnvfile,coverdiff,haplosize,procs,picardjar,mindepth,maxdepth,minmutreads,avoidreads,aligner|
+    if outbam.nil?
+      inputs[:outbam]= self.tmp_path
+    end
+    reference = reference_file reference
+    bam = Samtools.prepare_BAM(bam) if bam
+    reference = Samtools.prepare_FASTA(reference)
+    inputs[:reference] = reference
+    BAMSurgeon.add_snvs(inputs)
+  end
+
+
+  input :varfile, :file, "variants file in bed format", :nofile => true
+  input :bamfile, :file, "BAM", :nofile => true
+  input :outbam, :file, "Output BAM", nil, :nofile => true
+  input :reference, :select, "Reference code", "b37", :select_options => %w(b37 hg19 hg38 GRCh38 hs37d5), :nofile => true
+  input :maxlibsize, :float, "imaximum fragment length of seq. library"
+  input :kmer, :float, "kmer size for assembly", 31
+  input :svfrac, :float, "allele fraction of variant", 1.0
+  input :require_exact, :boolean, "drop mutation if breakpoints cannot be made exactly as input", false
+  input :minctglen, :float, "minimum length for contig generation, also used to pad assembly", 4000
+  input :maxmuts, :float, "maximum number of mutations to make ", nil
+  input :cnvfile, :file, "tabix-indexed list of genome-wide absolute copy number values", nil, :nofile => true
+  input :aligner, :select, "aligner", "mem", :select_options => %w(mem backtrack novoalign gsnap STAR bowtie2 tmap bwakit), :nofile => true
+
+  task :BAMSurgeon_add_struct_vars => :binary do |varfile,bamfile,outbam,reference,maxlibsize,kmer,svfrac,require_exact,minctglen,maxmuts,cnvfile,aligner|
+    if outbam.nil?
+      inputs[:outbam]= self.tmp_path
+    end
+    reference = reference_file reference
+    bam = Samtools.prepare_BAM(bam) if bam
+    reference = Samtools.prepare_FASTA(reference)
+    inputs[:reference] = reference
+    BAMSurgeon.add_struct_vars(inputs)
+  end
 end
--- lib/tools/BAMSurgeon.rb
+++ lib/tools/BAMSurgeon.rb
@@ -7,13 +7,20 @@ module BAMSurgeon
   
   Rbbt.claim Rbbt.software.opt.BAMSurgeon, :install, Rbbt.share.install.software.BAMSurgeon.find
 
-  def self.add_indels(bed,bam,reference,output,snv_frac,mut_frac,num_snvs,cnv_file,cover_diff,procs,picard_jar,min_depth,max_depth,min_mut_threads,avoid_reads,aligner)
+  def self.add_indels(args)
     addindels_cmd = Rbbt.software.opt.BAMSurgeon.bin["addindel.py"].find
-
-    CMD.cmd_log("#{addindels_cmd} -v #{bed} -f #{bam} -r #{reference} -o #{output} -s #{snv_frac} -m #{mut_frac} -n #{num_snvs} -c #{cnv_file} -d #{cover_diff} -p #{procs} --picardjar #{picard_jar} --mindepth #{min_depth} --maxdepth #{max_depth}  --minmutreads #{min_mut_threads} --avoidreads #{avoid_reads} --aligner #{aligner}")
+    CMD.cmd_log("#{addindels_cmd}", args.to_hash.merge('add_option_dashes' => true))
   end
 
+  def self.add_snvs(args)
+    addindels_cmd = Rbbt.software.opt.BAMSurgeon.bin["addsnv.py"].find
+    CMD.cmd_log("#{addindels_cmd}", args.to_hash.merge('add_option_dashes' => true))
+  end
 
+  def self.add_struct_vars(args)
+    addindels_cmd = Rbbt.software.opt.BAMSurgeon.bin["addsv.py"].find
+    CMD.cmd_log("#{addindels_cmd}", args.to_hash.merge('add_option_dashes' => true))
+  end
 end
 
 if __FILE__ == $0
