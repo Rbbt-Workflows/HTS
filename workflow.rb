4d47d4fe1891158205544f3e3c72e7a069a8f4f9
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2019-11-26 09:55:28 +0100

    parameter fix

--- workflow.rb
+++ workflow.rb
@@ -289,7 +289,7 @@ module HTS
     end
   end
 
-  helper :gatk do |command,args,sin=nil,tmp_dir|
+  helper :gatk do |command,args,sin=nil,tmp_dir=nil|
 
     if GATK::SPARK_COMMANDS.include?(command) and config('spark', :gatk, command) 
       args, fixed_files = fix_spark_args command, args
