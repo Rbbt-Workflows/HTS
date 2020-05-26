efb97b5f408a10d5d03725ed05869c95208fe14f
Hugo Jiménez Hernández <hjimenezhdez@gmail.com>
2020-05-26 11:38:11 +0200

    Intogen workflow revamped

--- lib/HTS/tasks/VCF/intogen.rb
+++ lib/HTS/tasks/VCF/intogen.rb
@@ -1,51 +1,52 @@
-module HTS 
+require 'pathname'
+module HTS
 
-  input :input, :file, "Input folder", nil, :nofile => true
-  input :resume, :bool, "Resume previous execution or start a new one", true
-  input :nextflow_script, :file,"Location of intogen nextflow script", nil, :nofile => true
+  input :input, :file, "Input folder with mafs and dataset.bginfo files", nil, :nofile => true
   input :intogen_reference, :select, "Reference code", "hg38", :select_options => %w(hg38 hg19), :nofile => true
-  input :conda_env, :file, "Condat environment path", nil, :nofile => true
-  task :intogen_pre => :text do |input, resume, nextflow_script, intogen_reference, conda_env|
-    files_dir= self.path + ".files"
-    nextflow_script = File.expand_path(nextflow_script)
-    conda_env = File.expand_path(conda_env)
-    input= File.expand_path(input)
+  task :intogen_pre => :text do |input, intogen_reference|
+    nextflow_script = config('NXFScript', :intogen)
+    conda_env = ENV["CONDA_PREFIX"]
+    if conda_env.nil?
+      raise "An activated conda environment with intogen dependencies is necessary to continue the execution"
+    end
+    input = File.expand_path(input)
+
     Misc.in_dir file('output') do
       FileUtils.mkdir_p("work/conda/")
-      FileUtils.mkdir_p(self.path + ".results/")
       FileUtils.ln_s  "#{conda_env}", file('output') + "/work/conda" unless Dir.exist?file('output') + "/work/conda/#{File.basename(conda_env)}"
-      cmd_str ="export INTOGEN_VEP=vep92; export INTOGEN_RELEASE=v20191009; export INTOGEN_HOME=#{File.dirname(nextflow_script)}; export INTOGEN_GENOME=#{intogen_reference}; export LC_ALL=C.UTF-8; export LANG=C.UTF-8; "
-      cmd_str.concat("~/nextflow run $(realpath #{nextflow_script})")
+      cmd_str ="export INTOGEN_VEP=vep92; export INTOGEN_RELEASE=v20191009; export INTOGEN_HOME=#{File.dirname(nextflow_script)}; \
+      export INTOGEN_GENOME=#{intogen_reference}; export LC_ALL=C.UTF-8; export LANG=C.UTF-8; "
+      cmd_str += "nextflow run  #{nextflow_script} -resume "
       args = {}
       args["--input"] = input
-      args["--output"] = self.path + ".results"
-      if resume
-        cmd_str.concat(" --resume")
-      end
+      args["--output"] = file("results")
+
       CMD.cmd_log(cmd_str, args)
-    end 
-    FileUtils.ln_s Dir.glob(self.path + ".results/oncodriveclustl/*.out.gz")[0], self.path
+    end
+    FileUtils.touch file("results") + "/done"
+    FileUtils.ln_s file("results") + "/done", self.path
     nil
   end
-  
+
   dep :intogen_pre
-  task :intogen_combination => :text do 
-    intogen_pre_path=step(:intogen_pre).path
-    inputs = step(:intogen_pre).inputs
-    container = File.expand_path(Dir.glob(File.dirname(inputs[:nextflow_script]) + "/containers/#{inputs[:intogen_reference]}*/combination.simg")[0])
-    output_path = intogen_pre_path + ".results/combination"
+  task :intogen_combination => :text do
+    intogen_pre_path = step(:intogen_pre).path
+    nextflow_script = config('NXFScript', :intogen)
+    results_path = intogen_pre_path + ".files/results"
+    output_path = results_path + "/combination"
+
     FileUtils.mkdir_p(output_path)
-    run_identifier = File.basename(Dir.glob(intogen_pre_path + ".results/signature/*.in.gz")[0],".in.gz")
-    datasets_dir = Dir.glob(File.dirname(File.expand_path(inputs[:nextflow_script]))+"/datasets/#{inputs[:intogen_reference]}*").first
-    containers_dir =  Dir.glob(File.dirname(File.expand_path(inputs[:nextflow_script]))+"/containers/#{inputs[:intogen_reference]}*").first
-    Misc.in_dir output_path do
-      cmd_str = "export INTOGEN_DATASETS=#{datasets_dir}; export INTOGEN_CONTAINERS=#{containers_dir}; "
-      cmd_str += "#{container} #{output_path} #{run_identifier}"
+    datasets_path = Dir.glob(File.dirname(nextflow_script) + "/datasets/#{step(:intogen_pre).inputs[:intogen_reference]}*").first
+    combination_container_path = Dir.glob(File.dirname(nextflow_script) + "/containers/#{step(:intogen_pre).inputs[:intogen_reference]}*/combination.simg").first
+    run_identifier = File.basename(Dir.glob(results_path + "/signature/*.in.gz")[0],".in.gz")
+
+    Misc.in_dir  output_path do
+      cmd_str = "export INTOGEN_DATASETS=#{datasets_path}; export INTOGEN_CONTAINERS=#{Pathname.new(combination_container_path).dirname.to_s}; "
+      cmd_str += "#{combination_container_path} #{output_path} #{run_identifier}"
       CMD.cmd_log(cmd_str)
-      FileUtils.mkdir_p(self.path+".files")
-      FileUtils.cp Dir.glob(output_path + "/*.gz"), self.path + ".files/"
     end
     FileUtils.ln_s output_path + "/#{run_identifier}.stouffer.out.gz", self.path
     nil
   end
+
 end
