3a35bc2f15e2ff107e1637c918166aa2263bcf27
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2022-01-18 14:00:37 +0100

    Improve pyclone and add cancer_cell_fraction

--- lib/HTS/tasks/pyclone.rb
+++ lib/HTS/tasks/pyclone.rb
@@ -1,14 +1,17 @@
+Workflow.require_workflow "Sequence"
+
 module HTS
   CMD.tool "pyclone-vi" do
     'pip install git+https://github.com/Roth-Lab/pyclone-vi.git'
   end
 
-
   input :copynumber, :tsv, "Copy number segments by sequenza"
   input :sample_name, :string, "Sample name"
+  input :tumor_content, :float, "Tumor content", 1
   dep HTS, :mutation_pileup
-  task :pyclone_variant_info => :tsv do |copynumber,sample_name|
-    dumper = TSV::Dumper.new :key_field => "mutation_id", :fields => ["sample_id", "ref_counts", "alt_counts", "major_cn", "minor_cn", "normal_cn"], :type => :list
+  dep Sequence, :expanded_vcf, :compute => :produce
+  task :pyclone_variant_info => :tsv do |copynumber,sample_name, tumor_content|
+    dumper = TSV::Dumper.new :key_field => "mutation_id", :fields => ["sample_id", "ref_counts", "alt_counts", "normal_cn", "minor_cn", "major_cn", "tumor_content"], :type => :list
     dumper.init(:preamble => "", :header_hash => '')
 
     sample_name ||= clean_name
@@ -21,17 +24,22 @@ module HTS
       k.split(":")[2].to_i
     end
 
+    vcf_info = step(:expanded_vcf).load.to_list
+    af_field = vcf_info.fields.select{|f| ! f.include?("normal") && f.split(":").last == "AD" }.first
+    af_pos = vcf_info.fields.index af_field
     cn_index = copynumber.range_index("Start", "End")
     TSV.traverse step(:mutation_pileup), :type => :array, :into => dumper do |line|
       chr, pos, ref, count, bases, *rest = line.split("\t")
 
-      mutation = rest.pop
+      if vcf_info[mutation]
+        ref_counts, alt_counts = vcf_info[mutation][af_pos].split(",")
+      else
+        base_counts = Misc.counts(bases.upcase.chars.select{|c| %w(A C T G).include? c })
+        chr, pos, alt = mutation.split(":")
 
-      base_counts = Misc.counts(bases.upcase.chars.select{|c| %w(A C T G).include? c })
-      chr, pos, alt = mutation.split(":")
-
-      alt_counts = base_counts[alt.upcase] 
-      ref_counts = (base_counts.values - [alt_counts]).max
+        alt_counts = base_counts[alt.upcase] 
+        ref_counts = (base_counts.values - [alt_counts]).max
+      end
       cn_key = cn_index[pos.to_i].select{|key| key.split(":").first.sub('chr', '') == chr.sub('chr', '')}.first
 
       if cn_key 
@@ -43,11 +51,52 @@ module HTS
         minor_cn = 1
       end
 
-      normal_cn = 2
+      if %w(y chry).include? chr.downcase
+        normal_cn = 1
+      else
+        normal_cn = 2
+      end
 
-      [mutation, [sample_name, ref_counts || 1, alt_counts || 1, major_cn, minor_cn, normal_cn]]
+      [mutation, [sample_name, ref_counts || 1, alt_counts || 1, normal_cn, minor_cn, major_cn, tumor_content]]
     end
   end
+
+  dep :pyclone_variant_info
+  dep Sequence, :affected_genes
+  task :cancer_cell_fraction => :tsv do
+    affected_genes = step(:affected_genes).load
+
+    tsv = step(:pyclone_variant_info).join.path.tsv :header_hash => "", :type => :list
+    tsv.add_field "Cancer cell fraction" do |mutation,values|
+      sample, ref, alt, normal, minor, major, tumor_content = values
+
+      tumor_content = 1 if tumor_content.nil? || tumor_content.empty?
+      p = tumor_content.to_f
+
+      cn = normal.to_i
+      ct = minor.to_i + major.to_i
+
+      vaf = alt.to_f / (ref.to_i + alt.to_i)
+      ccf = vaf / p * (p * ct + (1.0-p) * cn)
+
+      ccf
+    end
+
+    organism = TSV.get_stream(self.recursive_inputs[:organism]).read
+    ensg2name = Organism.identifiers(organism).index :target => "Associated Gene Name", :persist => true
+    tsv.add_field "Associated Gene Name" do |mutation,values|
+      ensg2name.values_at(*affected_genes[mutation]).compact
+    end
+  end
+
+  # VAF / p * (p * c_t + (1-p) * c_n)
+  # 
+  # where VAF is the alternate allele count divided by the total
+  # (alternate+reference) allele count, p is the purity and c_t (c_n) is the
+  # copy number or ploidy of the tumor (normal) at the mutated site.
+  # Alternatively, some of the phylogenetic reconstruction algorithms compute
+  # CCF as well.
+
 
 
   dep :pyclone_variant_info
