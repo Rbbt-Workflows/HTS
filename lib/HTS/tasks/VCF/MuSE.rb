24dd4c2c954785a041b3c33c7f1a87ce317970ee
Miguel Vazquez <miguel.vazquez.g@bsc.es>
2023-01-09 12:51:23 +0100

    Include option to not use dbSNP on muse

--- lib/HTS/tasks/VCF/MuSE.rb
+++ lib/HTS/tasks/VCF/MuSE.rb
@@ -7,8 +7,9 @@ module HTS
   input :normal, :file, "Normal BAM (optional)", nil, :nofile => true
   input :reference, :select, "Reference code", "hg38", :select_options => %w(b37 hg38 mm10), :nofile => true
   input :type_of_sequencing, :select, "Whole genome or whole exome", nil, :select_options => %w(WGS WES panel)
+  input :use_dbNSP, :boolean, "Use DbSNP common variants in muse", true
   extension :vcf
-  task :muse => :text do |tumor,normal, reference,type_of_sequencing|
+  task :muse => :text do |tumor,normal, reference,type_of_sequencing,use_dbNSP|
 
     reference_code = File.basename(reference).sub('.fa','').sub('.gz','')
     reference = reference_file reference
@@ -81,8 +82,8 @@ module HTS
     end
 
     build = Organism.GRC_build reference_code
-    if build
-      dbsnp = DbSNP[build]["common.vcf.gz"].find
+    if build && use_dbNSP
+      dbsnp = DbSNP[build]["common.vcf.gz"].produce.find
       dbsnp = GATK.prepare_VCF dbsnp
       CMD.cmd_log(:MuSE, "sump -I #{int_file}.MuSE.txt #{type_of_sequencing.to_s == "WGS" ? "-G" : "-E" } -O #{self.tmp_path} -D #{dbsnp}")
     else
